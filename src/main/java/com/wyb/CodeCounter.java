418fb440d12369be822631e9c8725a39960a408b
ouwybdboy <1977655749@qq.com>
2019-01-22 10:18:04 +0800

    代码行数统计

---
+++ src/main/java/com/wyb/CodeCounter.java
@@ -0,0 +1,66 @@
+package com.wyb;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by wuyb3 on 2019/1/22.
+ * https://www.cnblogs.com/bekeyuan123/p/6955283.html
+ */
+public class CodeCounter {
+    private static Integer code = 0;
+    private static Integer codeComments = 0;
+    private static Integer codeBlank = 0;
+
+    public static void main(String[] args) {
+        File file = new File("S:\\code\\src\\main\\java");
+        factFiles(file);
+        System.out.println("代码行数" + code);
+        System.out.println("空白行数" + codeBlank);
+        System.out.println("注释行数" + codeComments);
+    }
+
+    public static void factFiles(File file) {
+        BufferedReader br = null;
+        String s = null;
+
+        if(file.isDirectory()) {
+            File[] files = file.listFiles();
+            for(File f : files) {
+                factFiles(f);
+            }
+        } else {
+            try {
+                br = new BufferedReader(new FileReader(file));
+                boolean comm = false;
+                while((s = br.readLine()) != null) {
+                    if(s.startsWith("/*") && s.endsWith("*/")) {
+                        codeComments++;
+                    } else if(s.trim().startsWith("//")) {
+                        codeComments++;
+                    } else if(s.startsWith("/*") && !s.endsWith("*/")) {
+                        codeComments++;
+                        comm = true;
+                    } else if(!s.startsWith("/*") && s.endsWith("*/")) {
+                        codeComments++;
+                        comm = false;
+                    } else if(comm) {
+                        codeComments++;
+                    } else if(s.trim().length() < 1) {
+                        codeBlank++;
+                    } else {
+                        code++;
+                    }
+                }
+                br.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
